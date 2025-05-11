61295f9312c465517d41718bd3b79266d0581019
satyabratapradhan <satyabratapradhann@gmail.com>
2025-05-12 01:24:03 +0530

    add

---
+++ js/dataTypes.js
@@ -0,0 +1,14 @@
+"use strict" //treat all js code as newer vertion
+
+// alert( 3 + 3 ) //we are using node.js, not browser
+
+let name = "satya";
+let age = 21;
+let isLoggedIn = false;
+
+
+// number => 2 to power 53
+// bigint
+//string
+// bolean => true/false
+//null => standalone value and null type is a object
