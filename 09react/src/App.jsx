2da2e4af43c17dcfd62982f2d999debf3d5f47ab
satyabratapradhan <satyabratapradhann@gmail.com>
2025-05-25 23:09:56 +0530

    netflixSeries project

--- 09react/src/App.jsx
+++ 09react/src/App.jsx
@@ -1,19 +1,16 @@
 import { useState, version } from 'react'
-import NetflixSeries, {Footer, Header} from './components/NetflixSeries';
+import NetflixSeries from './components/NetflixSeries';
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 // import './App.css'
 
 function App() {
   const [count, setCount] = useState(0);
-  const img = "https://www.comingsoon.net/wp-content/uploads/sites/3/2024/01/Untitled-design-2024-01-31T102823.476.jpg"
   return (
     <>
-    <Header />
+    
     <NetflixSeries />
-    <NetflixSeries />
-    <NetflixSeries />
-    <Footer />
+   
     </>
   )
 }
--- 09react/src/components/NetflixSeries.jsx
+++ 09react/src/components/NetflixSeries.jsx
@@ -1,23 +1,25 @@
- const NetflixSeries = () => {
+import seriesData from "../api/seriesData.json";
 
-  const name = "Queen Of Tears";
-  const rating = "8.2";
-  const sumary = "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corporis reprehenderit eos ut quod nisi, cumque aspernatur vel voluptatum qui, facilis, perspiciatis nesciunt vero. At quos minima consequuntur beatae nostrum a.";
+const NetflixSeries = () => {
 
-  const age = 18;
+  // const name = "Queen Of Tears";
+  // const rating = "8.2";
+  // const sumary = "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corporis reprehenderit eos ut quod nisi, cumque aspernatur vel voluptatum qui, facilis, perspiciatis nesciunt vero. At quos minima consequuntur beatae nostrum a.";
+
+  // const age = 18;
 
   // let canWatch = "Not Available";
   // if(age >= 18) canWatch = "Watch Now";
 
-  const returnGenre = () => {
-    const genre = "RomCom"
-    return genre;
-  }
+  // const returnGenre = () => {
+  //   const genre = "RomCom"
+  //   return genre;
+  // }
 
-  const canWatch = () => {
-    if(age >= 18) return "Watch Now";
-    return "Not Available"
-  }
+  // const canWatch = () => {
+  //   if(age >= 18) return "Watch Now";
+  //   return "Not Available"
+  // }
 
   // if(age < 18){
   //   return(
@@ -33,27 +35,36 @@
   // }
 
   return(
-    <div>
-        <img src ="https://www.comingsoon.net/wp-content/uploads/sites/3/2024/01/Untitled-design-2024-01-31T102823.476.jpg" alt="img" />
-        <h2>Name: {name}</h2>
-        <h3>Rating: {rating}</h3>
-        <p> Summary : {sumary} </p>
-        <p>Genre: {returnGenre()} </p>
-        <button>{canWatch()}</button> <br /><br /><br /><br />
-      </div>
+    <ul>
+      {seriesData.map((curElem) => {
+        return (
+          <li key={curElem.id}>
+        <div>
+        <img src = {curElem.img_url } alt = {curElem.name} width="30%" height="50%" />
+        </div>   
+        <h2>Name: {curElem.name}</h2>
+        <h3>Rating: {curElem.rating}</h3>
+        <p> Summary : {curElem.description} </p>
+        <p>Genre: {curElem.genre} </p>
+        <p>Cast: {curElem.cast}</p>
+        <a href= {curElem.watch_url} target="_blank"> <button>Watch Now</button> </a>
+      </li>
+        )
+      })}
+      </ul>
   )
 };
 
   export default NetflixSeries;
 
-export const Header = () => {
-    return (
-        <p>copyright @satyabratapradhan</p>
-    )
-}
+// export const Header = () => {
+//     return (
+//         <p>copyright @satyabratapradhan</p>
+//     )
+// }
 
-export const Footer = () => {
-    return (
-        <p>copyright @satyabratapradhan</p>
-    )
-}
+// export const Footer = () => {
+//     return (
+//         <p>copyright @satyabratapradhan</p>
+//     )
+// }
