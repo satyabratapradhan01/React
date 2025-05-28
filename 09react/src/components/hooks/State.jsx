183bdf1da3beff41de8f1f63eb7badb8e2e5c9ae
satyabratapradhan <satyabratapradhann@gmail.com>
2025-05-28 15:22:33 +0530

    react state rerender

--- 09react/src/components/hooks/State.jsx
+++ 09react/src/components/hooks/State.jsx
@@ -4,13 +4,23 @@ export const State = () => {
     const [ count, setCount] = useState(0)
 
     const incressNum = () => {
-        setCount(() => count + 1)
+        setCount(() => count + 1 )
     }
 
     return(
         <>
         <h1>{count}</h1>
         <button onClick={incressNum}>Click Me</button>
+        <ChildComponent count={count}/>
         </>
+    );
+};
+
+function ChildComponent({ count }) {
+    console.log("Child Component rendered");
+    return(
+        <div>
+            <h2>Child Componebt = {count}</h2>
+        </div>
     )
 }
