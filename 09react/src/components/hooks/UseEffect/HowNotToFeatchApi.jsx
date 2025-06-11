030fbfb76d53655375836443d3c6cad87d13f8af
satyabratapradhan <satyabratapradhann@gmail.com>
2025-06-11 22:57:16 +0530

    pokemon project

--- 09react/src/components/hooks/UseEffect/HowNotToFeatchApi.jsx
+++ 09react/src/components/hooks/UseEffect/HowNotToFeatchApi.jsx
@@ -1,28 +1,47 @@
 import { useState, useEffect } from "react";
+import "./Pokemon.css";
 
 export const HowNotToFeatchApi = () => {
-  const [apiData, setApiData] = useState([]);
-  const API = "https://jsonplaceholder.typicode.com/posts";
-;
+  const [apiData, setApiData] = useState(null);
+  const API = "https://pokeapi.co/api/v2/pokemon/squirtle";
 
-  useEffect(() => {
+  const fetchPokemon = () => {
     fetch(API)
       .then((res) => res.json())
       .then((data) => {
         setApiData(data);
       })
       .catch((error) => console.log(error));
+  }
+  useEffect(() => {
+    fetchPokemon();
   }, []);
 
+
+  if(!apiData)
+    return(
+  <div>
+    <h1>Loading.....</h1>
+  </div>
+    )
+
+if (apiData) {
   return (
-    <>
-    <div>
-        <ul> Data:
-            {apiData.map((curData) => {
-                return <li key={curData.id}> {curData.title}</li>
-            })}
-        </ul>
-    </div>
-    </>
+    <section className="container">
+      <header>
+        <h1>Lets Catch Pokemon</h1>
+      </header>
+      <ul className="card-demo">
+        <li className="pokemon-card">
+          <figure>
+            <img src={apiData.sprites.other.dream_world.front_default} 
+            alt={apiData.name} 
+            className="pokemon-image"/>
+          </figure>
+          <h1>{apiData.name}</h1>
+        </li>
+      </ul>
+    </section>
   );
+}
 };
