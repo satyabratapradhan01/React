ae5287604c9b341c783404e14c252e7911c4bc49
satyabratapradhan <satyabratapradhann@gmail.com>
2025-06-16 11:03:26 +0530

    react rought dom

--- React_Router/src/App.jsx
+++ React_Router/src/App.jsx
@@ -4,38 +4,45 @@ import About from './Pages/About';
 import Contact from './Pages/Contact';
 import Home from './Pages/Home';
 import Movie from './Pages/Movie';
+import AppLayout from './components/layout/AppLayout';
 
 function App() {
 
-  // const router = createBrowserRouter([
-  //     {
-  //       path: "/",
-  //       element: <Home />
-  //     },
-  //     {
-  //       path: "/about",
-  //       element: <About />
-  //     },
-  //     {
-  //       path: "/movie",
-  //       element: <Movie />
-  //     },
-  //     {
-  //       path: "/contact",
-  //       element: <Contact />
-  //     },
-  //   ])
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <AppLayout />,
+      children: [
+        {
+        path: "/",
+        element: <Home />
+      },
+      {
+        path: "/about",
+        element: <About />
+      },
+      {
+        path: "/movie",
+        element: <Movie />
+      },
+      {
+        path: "/contact",
+        element: <Contact />
+      },
+      ]
+    }
+    ])
 
-  const router = createBrowserRouter(
-    createRoutesFromElements(
-      <Route>
-        <Route path='/' element={<Home />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/movie' element={<Movie />} />
-        <Route path='/contact' element={<Contact />} />
-      </Route>
-    )
-  )
+  // const router = createBrowserRouter(
+  //   createRoutesFromElements(
+  //     <Route>
+  //       <Route path='/' element={<Home />} />
+  //       <Route path='/about' element={<About />} />
+  //       <Route path='/movie' element={<Movie />} />
+  //       <Route path='/contact' element={<Contact />} />
+  //     </Route>
+  //   )
+  // )
 
 
   return (
---
+++ React_Router/src/components/layout/AppLayout.jsx
@@ -0,0 +1,15 @@
+import { Outlet } from "react-router-dom";
+import Footer from "./Footer";
+import Header from "./Header";
+
+function AppLayout() {
+    return ( 
+        <>
+        <Header />
+        <Outlet />
+        <Footer />
+        </>
+     );
+}
+
+export default AppLayout;
